71d1e2d819ce97699aff8b30e64248f7d57ab6ae
dvtate <toast27@gmail.com>
2022-06-03 22:54:57 -0500

    fixes

--- frontend/src/lib/globals.ts
+++ frontend/src/lib/globals.ts
@@ -7,7 +7,7 @@ export const API_SERVER_URL = 'http://xssaas.com/api';
 /**
  * URL to connect to the Router server
  */
-export const ROUTER_WS_URL = 'ws://localhost:6333';
+export const ROUTER_WS_URL = 'ws://xssaas.com:6333';
 
 /**
  * Disable location/ip sharing
